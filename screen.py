2dc9fe906052a30617c7514ffc38f4e1e1b9d821
VegetableDogKing <vincent97295@gmail.com>
2024-05-28 16:23:15 +0800

    weather

--- screen.py
+++ screen.py
@@ -2,7 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import Toplevel
-from datetime import datetime
+from datetime import datetime, timedelta
+from tkcalendar import Calendar, DateEntry
 
 def show_error_message(title, message):
     # Create a new Toplevel window for the custom messagebox
@@ -19,54 +20,55 @@ def show_error_message(title, message):
     ok_button.pack(pady=10)
 
 def start_button_click():
-    mood = mood_entry.get()
-    date = date_entry.get()
-
-    # Validate date
-    try:
-        datetime.strptime(date, '%Y/%m/%d')
-    except ValueError:
-        show_error_message("錯誤日期", "請按照 YYYY/MM/DD 的方式輸入日期")
-        return
+    
+    
 
     # Hide the input frame
     input_frame.pack_forget()
 
     # Show the result frame
-    result_label.config(text=f"Mood: {mood}\nDate: {date}\nWeather and outfit recommendation will be displayed here.")
+    
     result_frame.pack(pady=20)
 
 # Create main window
 root = tk.Tk()
-root.title("穿搭")
+root.title("讓我來尋找您的一日穿搭")
 root.geometry("1280x720")
-root.configure(background='black')
+root.configure(background='#FFF0F5')
 
 # Define a style for larger fonts
 style = ttk.Style()
-style.configure("TLabel", font=("Arial", 50))
-style.configure("TEntry", font=("Helvetica", 24))
-style.configure("TButton", font=("Arial", 50))
-style.configure('W.TCombobox',arrowsize = 23)
+style.configure("TEntry", font=("Helvetica", 30))
+style.configure("TButton", font=("Arial", 30), foreground="#CD5C5C")
+style.configure('TCombobox',arrowsize = 30)
+style.configure('TListbox', font=('Arial', 30))
+style.configure('TFrame', background="#FFF0F5")
 
 # Input frame
-input_frame = ttk.Frame(root)
-input_frame.pack(pady=20)
-
-# Mood entry
-mood_label = ttk.Label(input_frame, text="輸入你的心情：")
-mood_label.pack(pady=5)
-mood_entry = ttk.Entry(input_frame, width=50, font=('Arial 24'))
-mood_entry.pack(pady=5)
+input_frame = ttk.Frame(root, width=1000, height=500)
+input_frame.pack(pady=150)
+# input_frame.pack_propagate(False)
+input_frame.config(style="TFrame")
 
 # Date entry
-date_label = ttk.Label(input_frame, text="輸入日期(YYYY/MM/DD)：")
-date_label.pack(pady=5)
-date_entry = ttk.Entry(input_frame, width=50, font=('Arial 24'))
-date_entry.pack(pady=20)
+date_label = ttk.Label(input_frame, text="選擇日期(YYYY/MM/DD)：")
+date_label.config(background="#FFF0F5", font=("Arial", 30))
+date_label.pack()
+today = datetime.today().strftime('%Y-%-m-%-d %H:%M:%S').split(' ')[0].split('-')
+end_date = (datetime.today() + timedelta(days=11)).strftime('%Y-%-m-%-d %H:%M:%S').split(' ')[0].split('-')
 
-box = ttk.Combobox(input_frame, values=['臺北市', '新北市', '桃園市', '臺中市', '臺南市', '高雄市', '基隆市', '新竹縣', '新竹市', '苗栗縣', '彰化縣', '南投縣', '雲林縣', '嘉義縣', '嘉義市', '屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '澎湖縣', '金門縣', '連江縣'], width=49, font=('Arial 24'), style='W.TCombobox')
-box.pack(pady=5)
+mindate = datetime.today()
+maxdate = mindate + timedelta(days=11)
+
+cal = DateEntry(input_frame, selectmode='day',showweeknumbers=False, mindate=mindate, maxdate=maxdate, year=int(today[0]), month=int(today[1]), day=int(today[2]), weekendbackground='#FFFFFF', selectforeground='#FFFFFF', date_pattern='yyyy/mm/dd')
+cal.config(font=("Arial", 30))
+cal.pack(pady=20)
+
+area_label = ttk.Label(input_frame, text="選擇地區：")
+area_label.config(background="#FFF0F5", font=("Arial", 30))
+area_label.pack(pady=5)
+box = ttk.Combobox(input_frame, values=['臺北市', '新北市', '桃園市', '臺中市', '臺南市', '高雄市', '基隆市', '新竹縣', '新竹市', '苗栗縣', '彰化縣', '南投縣', '雲林縣', '嘉義縣', '嘉義市', '屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '澎湖縣', '金門縣', '連江縣'], width=12, font=('Arial 30'))
+box.pack(pady=20)
 
 # Start button
 start_button = ttk.Button(input_frame, text="開始！", command=start_button_click)
---
+++ weather.py
@@ -0,0 +1,69 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import webbrowser
+url = 'https://tw.news.yahoo.com/weather/%E8%87%BA%E7%81%A3/%E8%87%BA%E5%8C%97%E5%B8%82/%E8%87%BA%E5%8C%97%E5%B8%82-2306179' 
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+#打開要去的縣市
+#臺北雲林新北嘉義基隆臺南桃園高雄新竹屏東苗栗宜蘭臺中花蓮彰化臺東南投
+elements = soup.find_all('li', class_='My(10px)')
+city_links = {}
+for element in elements:
+    city_name = element.text.strip()
+    city_link = element.find('a')['href']
+    city_links[city_name] = city_link
+search_city = input("想查詢之縣市:") 
+if search_city in city_links:
+    city_url = city_links[search_city]
+    full_url = 'https://tw.news.yahoo.com' + city_url 
+    response = requests.get(full_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+else:
+    print(f"未找到名為 {search_city} 的縣市")
+    
+# 查找所有具有特定class的<dd>元素
+elements = soup.find_all('dd', class_='Mstart(5px) W(25px) Lh(2rem)')
+data = []
+for element in elements:
+    text = element.text.strip()
+    if text.endswith('%'):
+        text = text[:-1]  
+    try:
+        number = int(text) 
+        data.append(number)
+    except ValueError:
+        print(f"No data: {text}")
+        continue
+#high temp
+elements = soup.find_all('dd', class_='D(n) celsius_D(b) W(25px)')
+data_high = []
+for element in elements:
+    text = element.text.strip()
+    if text.endswith('°'):
+        text = text[:-1] 
+    try:
+        number = int(text) 
+        data_high.append(number)
+    except ValueError:
+        print(f"No data: {text}")
+        continue
+#low temp
+elements = soup.find_all('dd', class_='Pstart(10px) C($lowTemperatureColor) D(n) celsius_D(b) W(25px)')
+data_low = []
+for element in elements:
+    text = element.text.strip()
+    if text.endswith('°'):
+        text = text[:-1]  
+    try:
+        number = int(text)  
+        data_low.append(number)
+    except ValueError:
+        print(f"No data: {text}")
+        continue
+df = pd.DataFrame({
+    'Low_temp': data_low,
+    'High_temp': data_high,
+    'Raining possibility': data
+})
+print(df)
